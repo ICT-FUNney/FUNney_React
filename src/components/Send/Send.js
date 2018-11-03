e66f8c3b8099b5a39a837338a8887e2c0f92186f
b1018043 <b1018043@fun.ac.jp>
2018-11-03 18:09:30 +0900

    use material-ui and make indent

--- src/components/Send/Send.js
+++ src/components/Send/Send.js
@@ -1,37 +1,71 @@
-import React from 'react'
-import './Send.css'
+import React from 'react';
+import { withStyles } from '@material-ui/core/styles';
+import MenuItem from '@material-ui/core/MenuItem';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
 
-const Send=()=>{
-	let studentNum=''
-	let money=''
+const styles = theme => ({
+  container: {
+    display: 'flex',
+    flexWrap: 'wrap',
+  },
+  textField: {
+    marginLeft: theme.spacing.unit,
+    marginRight: theme.spacing.unit,
+    width: 200,
+  },
+  menu: {
+    width: 200,
+  },
+  button: {
+    margin: theme.spacing.unit,
+  },
+  input: {
+    display: 'none',
+  },
+});
 
-	return(
-		<div className='box'>
-			<form
-				onSubmit={e=>{
-				e.preventDefault()
-				if(!studentNum.value.trim()){
-					return
-				}
-				if(!money.value.trim()){
-					return
-				}
-				studentNum.value=''
-				money.value=''
-			}}
-			>
-				<div>
-					<input placeholder="送り先 学籍番号" ref={node=>studentNum=node}/>
-				</div>
-				<div>
-					<input placeholder="送金金額" ref={node=>money=node}/>
-				</div>
-				<button type="submit">
-					Send
-				</button>
+class Senda extends React.Component {
+	state = {
+		studentNum:'',
+		sendMoney: '',
+  };
+
+	handleChange = name => event => {
+    this.setState({
+      [name]: event.target.value,
+    });
+  };
+
+	render(){
+		const {classes}=this.props;
+
+		return (
+			<form className={classes.container} noValidate autoComplete="off">
+				<TextField
+					id="studentNum"
+					label="送り先 学籍番号"
+					placeholder="学籍番号を入力してください"
+					className={classes.textField}
+	 				onChange={this.handleChange('studentNum')}
+					value={this.state.studentNum}
+					margin="normal"
+				/>
+				<TextField
+					id="sendMoney"
+					label="送金金額"
+					placeholder="金額を入力してください"
+					className={classes.textField}
+					onChange={this.handleChange('sendMoney')}
+					value={this.state.sendMoney}
+					margin="normal"
+				/>
+        <Button variant="contained" color="primary" className={classes.button}>
+          送金
+        </Button>
 			</form>
-		</div>
-	)
+		);
+	}
 }
 
-export default Send
+export default withStyles(styles)(Senda);
