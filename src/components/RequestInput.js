ec7d29eb16384dd531c62ec77e6c1069836e7ffd
渡邉雄之介 <watanabeyunosuke@yus.local>
2018-11-12 15:57:42 +0900

    半角数字の時のみ遷移できるようにしました。

--- src/components/RequestInput.js
+++ src/components/RequestInput.js
@@ -13,6 +13,21 @@ const styles = theme => ({
 
 });
 
+const requestMethod = [
+  {
+    value: 'noselect',
+    label: '選択してください',
+  },
+  {
+    value: 'QR',
+    label: 'QRコード',
+  },
+  {
+    value: 'mail',
+    label: 'メール',
+  },
+];
+
 
 const RequestInput = (props) => {
 
@@ -22,27 +37,54 @@ const RequestInput = (props) => {
 　　　　 <div>
         <TextField
          id="standard-uncontrolled"
-         helperText ={(props.errorFlagName)? "error": ""}
-         error={props.errorFlagName}
+         error={props.errorFlagNameNumber || props.errorFlagName}
+         helperText ={(props.errorFlagName)? "error": (props.errorFlagNameNumber)? "error 半角数字のみ入力してください" : ""}
          label="要求金額"
          defaultValue=""
          className={classes.textField}
+         value={props.name}
          onChange={e=>{props.handleChange(e)}}
          margin="normal"
          name="name"
         />
       </div>
+      <div>
         <TextField
           id="standard-name"
-          helperText ={(props.errorFlagDefaultValue)? "error": ""}
-          error={props.errorFlagDefaultValue}
+          helperText ={(props.errorFlagDefaultValue)? "error":(props.errorFlagDefaultValueNumber) ? "error 半角数字のみ入力してください" : ""}
+          error={props.errorFlagDefaultValueNumber || props.errorFlagDefaultValue}
           label="送り先学籍番号"
           className={classes.textField}
-          value={props.name}
+          value={props.defaultValue}
           onChange={e=>{props.handleChange(e)}}
           margin="normal"
           name="defaultValue"
         />
+        </div>
+        <TextField
+         id="standard-select-currency"
+         helperText ={(props.errorFlagRequestSelect)? "error": ""}
+         error={props.errorFlagRequestSelect}
+         select
+         label="要求方法"
+         className={classes.textField}
+         value={props.requestSelect}
+         onChange={e=>{props.handleChange(e)}}
+         SelectProps={{
+           MenuProps: {
+             className: classes.menu,
+           },
+         }}
+
+         margin="normal"
+         name="requestSelect"
+       >
+         {requestMethod.map(option => (
+           <MenuItem key={option.value} value={option.value}>
+             {option.label}
+           </MenuItem>
+         ))}
+       </TextField>
       </form>
     );
   }
--- src/containers/Request.js
+++ src/containers/Request.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from '../logo.svg';
 import RequestInput from '../components/RequestInput.js';
 import RequestBotan from '../components/RequestBotan.js';
 import '../App.css'
@@ -9,25 +8,33 @@ class Request extends Component {
     super(props);
     this.state= {
       name: "",
+      defaultValue:"",
+      requestSelect:"noselect",
       errorFlagName: false,
       erroFlagDefaultValue:false,
-      defaultValue:"",
+      errorFlagRequestSelect:false,
+      errorFlagNameNumber:false,
+      errorFlagDefaultValueNumber:false,
+
     }
   }
 
-  clickSendRequest(){
-  if(this.state.name === "" ){
-    this.setState({
-    errorFlagName: true,
-    })
-  }else if(this.state.defaultValue === ""){
-    this.setState({
-    errorFlagDefaultValue: true,
-    })
-  }else{
-    this.props.history.push('/home');
-  }
-}
+clickSendRequest(){
+     const checkNumber = new RegExp(/^[0-9]+$/)
+     this.setState({
+     errorFlagName : (this.state.name !== "") ? false : true,
+     errorFlagDefaultValue : (this.state.defaultValue !== "") ? false : true,
+     errorFlagRequestSelect : (this.state.requestSelect !== "noselect") ? false : true,
+     errorFlagNameNumber: (checkNumber.test(this.state.name))? false : true,
+     errorFlagDefaultValueNumber: (checkNumber.test(this.state.defaultValue))? false : true,
+   })
+
+    if(this.state.name !== "" && this.state.defaultValue !== "" &&
+      this.state.requestSelect !== "noselect" && checkNumber.test(this.state.name)
+    && checkNumber.test(this.state.defaultValue)){
+      this.props.history.push('/home');
+      }
+    }
 
 handleChange = (e, name) => {
   this.setState({
@@ -42,8 +49,14 @@ handleChange = (e, name) => {
        <div>
         <div className="RequestInput">
         <RequestInput handleChange = {(e)=>{this.handleChange(e,e.target.name)}}
-         errorFlagName={this.state.errorFlagName}
-         errorFlagDefaultValue={this.state.errorFlagDefaultValue}
+          defaultValue={this.state.defaultValue}
+          name={this.state.name}
+          requestSelect={this.state.requestSelect}
+          errorFlagName={this.state.errorFlagName}
+          errorFlagDefaultValue={this.state.errorFlagDefaultValue}
+          errorFlagRequestSelect={this.state.errorFlagRequestSelect}
+          errorFlagNameNumber={this.state.errorFlagNameNumber}
+          errorFlagDefaultValueNumber={this.state.errorFlagDefaultValueNumber}
         />
        </div>
        <div className="RequestBotan">
