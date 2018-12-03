70b81f753e87bac06f0e1c62eac3253badb94bcd
kiksun <b1018005@fun.ac.jp>
2018-12-03 16:24:48 +0900

    総資産表示

--- src/containers/AppSend.js
+++ src/containers/AppSend.js
@@ -10,6 +10,7 @@ class AppSend extends Component {
 		submitModal: false,
 		errorEmptyNum: false,
 		errorEmptyMoney: false,
+		Propety: 100,
 	};
 	
 	handleChange =e=> {
@@ -65,6 +66,7 @@ class AppSend extends Component {
 		return (
 			<div className='Send'>
 				<Send
+					Propety={this.state.Propety}
 					sendSubmit={(e)=>{this.sendSubmit(e)}}
 					errorMoney={this.state.errorMoney}
 					errorNum={this.state.errorNum}
