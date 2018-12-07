44686eb3f222b8a925b4bdd267c3ee3d9698fc57
daisuke sato <satodsuke@gmail.com>
2018-12-08 00:07:36 +0900

    Home ユーザー名と総資産額の表示

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -3,22 +3,25 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import * as actions from '../actions/ActionCreators.js';
 
-import SendAndRequestButton from '../components/SendAndRequestButton.js';
+//import SendAndRequestButton from '../components/SendAndRequestButton.js';
+import HomeUserAndProperty from '../components/HomeUserAndProperty.js';
 
 class Home extends Component {
+  constructor(props){
+    super(props);
+    this.state={
+      user:"Tatsuki Ikai",
+      property:100,
+    }
+  }
+
   render() {
     console.log(this.props.test)
     return (
       <div className="Home">
-        <SendAndRequestButton
-        sendButton = {
-          ()=>{
-            this.props.addTest()
-          }}
-        clickButton ={
-          ()=>{
-            this.props.history.push('/signout');
-          }}
+        <HomeUserAndProperty
+          userID={this.state.user}
+          userProperty={this.state.property}
         />
       </div>
     );
